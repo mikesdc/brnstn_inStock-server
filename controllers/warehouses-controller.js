105237a5774be0cadcdeb5aacb5ffdce91974368
saksham1236 <saksham1236@gmail.com>
2023-11-07 22:35:39 -0500

    Adding endpoints for getting json data for single warehouse and single warehouse inventory.

--- controllers/warehouses-controller.js
+++ controllers/warehouses-controller.js
@@ -10,6 +10,72 @@ const index = (_req, res) => {
     );
 };
 
+/**
+ * Returns Json data for one warehouse in requested in req.params.warehouse_id : (warehouses/warehouse_id)
+ * @param {AxiosRequest} req 
+ * @param {AxiosResponse} res
+ * @returns {{id: number, warehouse_name: String, address: String, country: String, contact_name: String, contact_position: String, contact_phone: String,contact_email: String,created_at: EpochTimeStamp, updated_at: EpochTimeStamp} }
+ */
+const singleWarehouse = (req, res) => {
+  //Warehouse data knex query
+  knex('warehouses')
+  .where({ id: req.params.warehouse_id })
+  .then((warehousesFound) => {
+
+    //If warehouse not found
+    if(warehousesFound.length === 0) {
+      return res
+      .status(404)
+      .json({message: `Cannot find warehouse with id: ${req.params.warehouse_id}`})
+    }
+
+    //Response 200
+    const warehouseData = warehousesFound[0];
+    res.status(200).json(warehouseData);
+  })
+
+  //Catching errors, Gotta catch em all 🐉
+  .catch((error) => {
+    res.status(500)
+    .json({message: `Unable to retrieve data for warehouse: ${req.params.warehouse_id} 
+    failed with error: ${error}`})
+  });
+}
+
+/**
+ * Returns Json Array data for one warehouse inventory in requested in req.params.warehouse_id : (warehouses/warehouse_id)
+ * @param {AxiosRequest} req 
+ * @param {AxiosResponse} res
+ * @returns {[{"id": number(Primary Key),"warehouse_id": number(Foreign Key),"item_name": String, "description": String,"category": String, status : "Out of Stock" | "In Stock" , quantity": number,"created_at": EpochTimeStamp,"updated_at": EpochTimeStamp}, ...]}
+ */
+const singleWarehouseInventory = (req, res) => {
+  knex('inventories')
+  .where({warehouse_id: req.params.warehouse_id})
+  .then((inventoryFound) => {
+
+    //If inventory not found
+    if(inventoryFound === 0) {
+      return res
+      .status(404)
+      .json({message: `Cannot find inventory for warehouse id: ${req.params.warehouse_id}`})
+    }
+
+    //Response 200
+    const inventoryData = inventoryFound;
+    res.status(200).json(inventoryData);
+  })
+
+  //Catching errors, Gotta catch em all 🐉
+  .catch((error) => {
+    res.status(500)
+    .json({message: `Unable to retrieve data for warehouse: ${req.params.warehouse_id} 
+    failed with error: ${error}`})
+  })
+}
+
+
 module.exports = {
-  index
+  index,
+  singleWarehouse,
+  singleWarehouseInventory
 }
